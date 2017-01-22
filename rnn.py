e55081451e6c9738d2a20849a2ea69f0b85c6e11
samgregoost <samramasinghe@gmail.com>
2017-01-22 15:15:44 +0530

    bug fix

--- rnn.py
+++ rnn.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 
-file_path = '/home/sameera/Documents/Mphil/DBScanClustering/MotionTubeCode/matconvnet-1.0-beta20/staticvectors/v_diving_'
+file_path = '/home/sameera/Documents/Mphil/DBScanClustering/MotionTubeCode/matconvnet-1.0-beta20/finalvectors/v_diving_'
 saveName = 'v_diving_'
 
 
@@ -15,21 +15,21 @@ for x in range(1, 26):
 	    for k in range(1, 16):
             	fileName = file_path + '0' + str(x) + '_0' + str(y) + '_' + str(k)
 		saveNam = saveName + '0' + str(x) + '_0' + str(y)
-		if os.path.exists(fileName + '_static.mat'):
- 			inp = scipy.io.loadmat(fileName  + '_static.mat')
-			inputVector[k-1,:] = np.transpose(inp['scores'])
+		if os.path.exists(fileName + '.mat'):
+ 			inp = scipy.io.loadmat(fileName  + '.mat')
+			inputVector[k-1,:] = inp['valout']
 	    if np.count_nonzero(inputVector) > 0:
-	    	np.save('/home/sameera/Documents/Mphil/finalVectorsStatic/' + saveNam, inputVector)
+	    	np.save('/home/sameera/Documents/Mphil/finalVectors/' + saveNam, inputVector)
 	else:
 	    for k in range(1, 16):
             	fileName = file_path + str(x) + '_0' + str(y) + '_' + str(k)
 		print fileName
 		saveNam = saveName + str(x) + '_0' + str(y)
-		if os.path.exists(fileName + '_static.mat'):
-			inp = scipy.io.loadmat(fileName  + '_static.mat')
-			inputVector[k-1,:] = np.transpose(inp['scores'])
+		if os.path.exists(fileName + '.mat'):
+			inp = scipy.io.loadmat(fileName  + '.mat')
+			inputVector[k-1,:] = inp['valout']
 
 	    if np.count_nonzero(inputVector) > 0:
-	    	np.save('/home/sameera/Documents/Mphil/finalVectorsStatic/' + saveNam, inputVector)
+	    	np.save('/home/sameera/Documents/Mphil/finalVectors/' + saveNam, inputVector)
     	 
     
